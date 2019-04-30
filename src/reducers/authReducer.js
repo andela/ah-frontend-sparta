224062d68c5a99aba5395afbe4946105235d6017
felixkiryowa <franciskiryowa68@gmail.com>
2019-04-30 10:17:20 +0300

    feat(SetUp) Setting Redux In Created React Project
    - Setting up redux to be used in managing application states
    
    [Finishes #164798156]

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+// Initial reducer state
+const initialState = {
+  isAuthenticated: false,
+  user: {
+
+  },
+};
+
+export default function (state = initialState, action) {
+  switch (action) {
+    default:
+      return state;
+  }
+}
