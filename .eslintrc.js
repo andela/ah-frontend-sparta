ee81b2867db91be1b84bc1e936933bf4d8f31b11
felixkiryowa <franciskiryowa68@gmail.com>
2019-05-02 16:01:10 +0300

    #164798156 Redux setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:jest/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": ["plugin:jest/recommended",  "airbnb" ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react", "jest"
-    ],
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "jest/no-disabled-tests": "warn",
-        "jest/no-focused-tests": "error",
-        "jest/no-identical-title": "error",
-        "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error"
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "jest"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }]
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"]
+      }
     }
+  }
 };
