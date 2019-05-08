a7ca2a82c7b5d5781db10219ab36bf310957719b
felixkiryowa <franciskiryowa68@gmail.com>
2019-05-08 17:20:02 +0300

    #165879303 Fix heroku deploy issues (#19)

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './Inputbox.scss';
+import './InputBox.scss';
 
 const InputBox = ({ name, placeholder, type, onChangeInput, value }) => {
 
   return (
     <div className="inputContain">
-      <input type="text" type={type} value={value} name={name} placeholder={placeholder} className="input" onChange={val => onChangeInput(val)} />
+      <input  type={type} value={value} name={name} placeholder={placeholder} className="input" onChange={val => onChangeInput(val)} />
     </div>
   )
 }
